038df58adf778ffc5e8ecb61e9bc037b4087db52
Carlos H Brandt <c.brandt@jacobs-university.de>
2022-07-10 03:29:44 +0200

    remove _id reading from obj (not happening)

--- json_schema/schema_resolver.py
+++ json_schema/schema_resolver.py
@@ -5,31 +5,26 @@ import json
 import validate
 
 
-def resolve_references(obj, resolver, _id, refs=None):
+def resolve_references(obj, resolver, _id):
     # print(f"Input (obj): {obj}")
     # print(f"Input (id): {_id}")
-    # print(f"Input (refs): {refs}")
-    refs = refs if refs is not None else []
     if isinstance(obj, list):
-        out = [ resolve_references(item, resolver, _id, refs) for item in obj ]
+        out = [ resolve_references(item, resolver, _id) for item in obj ]
     elif isinstance(obj, dict):
-        _id = obj['$id'] if '$id' in obj else _id
+        # _id = obj['$id'] if '$id' in obj else _id
         # print(f"Is dict (obj): {obj}")
         # print(f"Is dict (id): {_id}")
         out = {}
         for k,v in obj.items():
             if k == "$ref":
-                # print(v)
                 if v.startswith('#'):
                     v = _id + v
                 else:
                     _id = v.split('#')[0]
-                # print(v)
                 res = resolver.resolve(v)[1]
-                refs.append(v)
-                out.update(resolve_references(res, resolver, _id, refs))
+                out.update(resolve_references(res, resolver, _id))
             else:
-                out[k] = resolve_references(v, resolver, _id, refs)
+                out[k] = resolve_references(v, resolver, _id)
     else:
         out = obj
 
@@ -45,12 +40,7 @@ def main(filename):
     schema_id = js['$id'] if '$id' in js else os.path.basename(filename)
     resolver = validate.create_resolver(schema_id)
 
-    refs = []
-    res = deepcopy(js)
-    while len(refs) == 0 or len(refs) != len(refs_old):
-        refs_old = refs[:]
-        res = resolve_references(res, resolver, schema_id, refs)
-    print(refs)
+    res = resolve_references(js, resolver, schema_id)
     return res
 
 
