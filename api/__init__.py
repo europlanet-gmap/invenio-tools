1c95acb48d93ff0e7468ef63aa18f0e0e7da38b2
Carlos H Brandt <c.brandt@jacobs-university.de>
2022-10-19 10:40:32 +0200

    Add gmap metadata form/app prototype

--- api/__init__.py
+++ api/__init__.py
@@ -1,6 +1,6 @@
 """
 Objects to read Astropedia, Planmap/Gmap pakages and publish on InvenioRDM
 """
-from .invenio import InvenioClient
-from .planmap import parse_package as parse_planmap_package
-from .astropedia import parse_astropedia_html
+# from .invenio import InvenioClient
+# from .planmap import parse_package as parse_planmap_package
+# from .astropedia import parse_astropedia_html
