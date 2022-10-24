79d6ab731dcd6b8016d2b82aa64776328ddb188f
Carlos H Brandt <c.brandt@jacobs-university.de>
2022-10-24 23:34:32 +0200

    Moved widgets creation and json-schema resolution outside Form

--- api/gui/form.py
+++ api/gui/form.py
@@ -5,37 +5,21 @@ from collections import UserDict
 from pathlib import Path
 from typing import Any, Union
 
-def read_planmap_json(metafile:Union[str,Path]) -> dict:
-    """Return JSON object from 'meta_file'"""
-    filename = Path(metafile)
-    with open(filename) as fp:
-        js = json.load(fp)
-    return js
+from . import schema2ipywidgets
 
 
-def set_data_to_form(json, form):
-    for field, value in json.items():
-        form.set(field, value)
+# def read_planmap_json(metafile:Union[str,Path]) -> dict:
+#     """Return JSON object from 'meta_file'"""
+#     filename = Path(metafile)
+#     with open(filename) as fp:
+#         js = json.load(fp)
+#     return js
 
-# layout = [
-#     'title',
-#     'shortname',
-#     'map_type',
-#     'target',
-#     [
-#         {"Longitude (west,east) [-180:180]": ['bbox_lon_west', 'bbox_lon_east']},
-#         {"Latitude (min,max) [-90:90]": ['bbox_lat_min', 'bbox_lat_max']}
-#     ],
-#     'publication_date',
-#     'doi',
-#     'authors_widgets',
-#     [
-#         {"Map description": ['description', 'aims', 'units', 'stratigraphic_info']},
-#         {"Spatial attributes": ['crs', 'output_scale']},
-#         {"Ancillary data": ['ancillary_data', 'related_products', 'heritage', 'extra_data']},
-#         {"Notes": ['standards', 'comments', 'acknowledge']}
-#     ]
-# ]
+
+# def set_data_to_form(json, form):
+#     for field, value in json.items():
+#         form.set(field, value)
+
 
 def assemble_widgets(form, layout):
     """Layout widgets"""
@@ -57,12 +41,10 @@ def assemble_widgets(form, layout):
 
 
 class Form(UserDict):
-    def __init__(self, json:Union[dict,None] = None, layout:Union[list,None] = None):
+    def __init__(self, schema:Union[dict,None] = None, layout:Union[list,None] = None):
         super().__init__()
-        _main = create_main_widgets()
-        _other = create_other_widgets()
-        self.update(_main)
-        self.update(_other)
+        wd = schema2ipywidgets.main(schema)
+        self.update(wd)
         self._widget = assemble_widgets(self, layout)
 
     def set(self, field:str, value:Any) -> bool:
@@ -72,266 +54,145 @@ class Form(UserDict):
         wdgt.value = value
 
     def add(self, field:str, value:Any) -> bool:
-        NotImplementedError
+        raise NotImplementedError
 
     def get(self, field:str) -> Any:
-        NotImplementedError
+        return self[field].value
 
     def read_json(self, filename):
-        NotImplementedError
+        raise NotImplementedError
 
     @property 
     def widget(self):
-        # form_main = widgets.TwoByTwoLayout(
-        #     top_left = widgets.VBox([
-        #         self['title'],
-        #         self['shortname'],
-        #         self['map_type'],
-        #         self['target'],
-        #         widgets.VBox([
-        #             widgets.Label("Longitude (west,east) [-180:180]"),
-        #             widgets.VBox([
-        #                 self['bbox_lon_west'],
-        #                 self['bbox_lon_east']
-        #             ])
-        #         ]),
-        #         widgets.VBox([
-        #             widgets.Label("Latitude (min,max) [-90:90]"),
-        #             widgets.VBox([
-        #                 self['bbox_lat_min'],
-        #                 self['bbox_lat_max']
-        #             ])
-        #         ]),
-        #         self['publication_date'],
-        #         self['doi'],
-        #         self['authors_widgets'],
-        #     ])
-        # )
-
-        # form_descr = widgets.Accordion(
-        #     children = [
-        #         widgets.VBox([
-        #             self['description'],
-        #             self['aims'],
-        #             self['units'],
-        #             self['stratigraphic_info'],
-        #         ]),
-        #         widgets.VBox([
-        #             self['crs'],
-        #             self['output_scale']
-        #         ]),
-        #         widgets.VBox([
-        #             self['ancillary_data'],
-        #             self['related_products'],
-        #             self['heritage'],
-        #             self['extra_data'],
-        #         ]),
-        #         widgets.VBox([
-        #             self['standards'],
-        #             self['comments'],
-        #             self['acknowledge'],
-        #         ])
-        #     ]
-        # )
-
-        # titles = [
-        #     "Map description",
-        #     "Spatial attributes",
-        #     "Ancillary data",
-        #     "Notes"
-        # ]
-
-        # for i,bx in enumerate(form_descr.children):
-        #     form_descr.set_title(i, titles[i])
-            
-        # form_descr.selected_index = None
-
-        app = widgets.AppLayout(
-            header = self['gmap_id'],
+        return widgets.AppLayout(
+            header = self['gmap_id'] if 'gmap_id' in self else None,
             center = self._widget,
             left_sidebar = None,
             right_sidebar = None,
             footer = None
         )
 
-        return app
 
 
+# widget_types = dict(
+#     text = widgets.Text,
+#     float = widgets.FloatText,
+#     datetime = widgets.DatePicker,
+#     items_one = widgets.Combobox,
+#     items_multiple = widgets.SelectMultiple,
+# )
 
-class Items(object):
-
-    def __init__(self, description="Text field"):
-        self.description = description
-        add_btn = widgets.Button(icon="plus")
-        add_btn.on_click(lambda btn: self.add_item())
-        self._widget = widgets.VBox([add_btn])
-        
-
-    def del_item(self, value):
-        def not_value(item):
-            try:
-                found = item.children[0].value == value
-            except:
-                found = False
-            return not found
-        
-        new_items = filter(not_value, self.widget.children)
-        self._widget.children = tuple(new_items)
-
-        return self
+# wt = widget_types
 
 
-    def add_item(self, value=None):
-        """Create new Text input widget in items"""
-        text = widgets.Text(description = self.description)
-        if value is not None:
-            text.value = str(value)
+# def create_main_widgets() -> dict:
+#     from datetime import date
 
-        del_btn = widgets.Button(icon="trash")
-        del_btn.on_click(lambda btn: self.del_item(text.value))
+#     map_types = {
+#         "Integrated": "I",
+#         "Morphologic": "M",
+#         "Compositional": "C",
+#         "Digital model": "D",
+#         "Stratigraphic": "S",
+#         "Geo-structural": "G",
+#     }
 
-        new_item = widgets.HBox([text, del_btn])
-        
-        if all(item.children[0].value.strip() 
-                for item in self.widget.children if hasattr(item, 'children')):
-            self.widget.children = tuple(list(self.widget.children) + [new_item])
+#     d = dict(
 
-        return self
-        
-        
-    @property
-    def widget(self):
-        return self._widget
+#         title = wt['text'](
+#             description = "Map title",
+#             continuous_update = False
+#         ),
 
-    @property 
-    def value(self):
-        value = [w[0].value for w in self.widget.children]
-        return value 
+#         target = wt['items_one'](
+#             options = ['Mars', 'Mercury', 'Moon', 'Venus'],
+#             placeholder = "Choose the planet/satelite",
+#             description = "Target body",
+#             continuous_update = False
+#         ),
 
-    @value.setter
-    def value(self, value:list):
-        for val in value:
-            self.add_item(val)
-        
+#         shortname = wt['text'](
+#             description = "Shortname",
+#             continuous_update = False
+#         ),
 
-widget_types = dict(
-    text = widgets.Text,
-    float = widgets.FloatText,
-    datetime = widgets.DatePicker,
-    items_one = widgets.Combobox,
-    items_multiple = widgets.SelectMultiple,
-)
+#         publication_date = wt['datetime'](
+#             value = date.today(),
+#             description = "Publication date"
+#         ),
 
-wt = widget_types
+#         map_type = wt['items_multiple'](
+#             options = sorted(map_types.keys()),
+#             description = "Map type"
+#         ),
 
-
-def create_main_widgets() -> dict:
-    from datetime import date
-
-    map_types = {
-        "Integrated": "I",
-        "Morphologic": "M",
-        "Compositional": "C",
-        "Digital model": "D",
-        "Stratigraphic": "S",
-        "Geo-structural": "G",
-    }
-
-    d = dict(
-
-        title = wt['text'](
-            description = "Map title",
-            continuous_update = False
-        ),
-
-        target = wt['items_one'](
-            options = ['Mars', 'Mercury', 'Moon', 'Venus'],
-            placeholder = "Choose the planet/satelite",
-            description = "Target body",
-            continuous_update = False
-        ),
-
-        shortname = wt['text'](
-            description = "Shortname",
-            continuous_update = False
-        ),
-
-        publication_date = wt['datetime'](
-            value = date.today(),
-            description = "Publication date"
-        ),
-
-        map_type = wt['items_multiple'](
-            options = sorted(map_types.keys()),
-            description = "Map type"
-        ),
-
-        authors = Items("Author (fullname)").widget,
+#         # authors = schema2ipywidgets.Items("Author (fullname)").widget,
+#         authors = schema2ipywidgets.Items("Author (fullname)"),
 
 
-        bbox_lon_west = wt['float'](description = "West-Lon"),
-        bbox_lon_east = wt['float'](description = "East-Lon"),
-        bbox_lat_min = wt['float'](description = "Min-Lat"),
-        bbox_lat_max = wt['float'](description = "Max-Lat"),
+#         bbox_lon_west = wt['float'](description = "West-Lon"),
+#         bbox_lon_east = wt['float'](description = "East-Lon"),
+#         bbox_lat_min = wt['float'](description = "Min-Lat"),
+#         bbox_lat_max = wt['float'](description = "Max-Lat"),
 
-        doi = wt['text'](description="DOI")
+#         doi = wt['text'](description="DOI")
 
-    )
+#     )
 
-    def create_gmapID_readonly(target_wgt, map_type_wgt, shortname_wgt, map_types_map):
-        gmap_id = wt['text'](
-            description = '<i style="color:gray">GMAP-ID</i>', 
-            description_allow_html = True,
-            disabled = True
-            )
+#     def create_gmapID_readonly(target_wgt, map_type_wgt, shortname_wgt, map_types_map):
+#         gmap_id = wt['text'](
+#             description = '<i style="color:gray">GMAP-ID</i>', 
+#             description_allow_html = True,
+#             disabled = True
+#             )
 
-        def on_change_value(change):
-            _target = target_wgt.value.strip().title()
-            _type = ''.join([map_types_map[t].upper() for t in map_type_wgt.value ])
-            _sname = shortname_wgt.value.strip().replace(' ','-').title()
-            _id = ["GMAP", _type, _target, _sname]
-            if not all(_id):
-                # gmap_id.value = "GMAP_<target>_<type>_<shortname>"
-                gmap_id.value = ""
-            else:
-                gmap_id.value = '_'.join(_id)
+#         def on_change_value(change):
+#             _target = target_wgt.value.strip().title()
+#             _type = ''.join([map_types_map[t].upper() for t in map_type_wgt.value ])
+#             _sname = shortname_wgt.value.strip().replace(' ','-').title()
+#             _id = ["GMAP", _type, _target, _sname]
+#             if not all(_id):
+#                 # gmap_id.value = "GMAP_<target>_<type>_<shortname>"
+#                 gmap_id.value = ""
+#             else:
+#                 gmap_id.value = '_'.join(_id)
 
-        target_wgt.observe(on_change_value, names='value')
-        shortname_wgt.observe(on_change_value, names='value')
-        map_type_wgt.observe(on_change_value, names='value')
+#         target_wgt.observe(on_change_value, names='value')
+#         shortname_wgt.observe(on_change_value, names='value')
+#         map_type_wgt.observe(on_change_value, names='value')
 
-        return gmap_id
+#         return gmap_id
 
-    d['gmap_id'] = create_gmapID_readonly(
-            target_wgt= d['target'],
-            map_type_wgt= d['map_type'],
-            shortname_wgt= d['shortname'],
-            map_types_map= map_types
-        )
+#     d['gmap_id'] = create_gmapID_readonly(
+#             target_wgt= d['target'],
+#             map_type_wgt= d['map_type'],
+#             shortname_wgt= d['shortname'],
+#             map_types_map= map_types
+#         )
 
-    return d
+#     return d
 
 
-def create_other_widgets():
+# def create_other_widgets():
 
-    d = dict(
-        description = widgets.Textarea(description="Short description of the map"),
-        aims = widgets.Textarea(description="Goal of this map"),
-        units = widgets.Textarea(description="Units color definition"),
-        stratigraphic_info = widgets.Textarea(description="Map stratigraphic elements"),
+#     d = dict(
+#         description = widgets.Textarea(description="Short description of the map"),
+#         aims = widgets.Textarea(description="Goal of this map"),
+#         units = widgets.Textarea(description="Units color definition"),
+#         stratigraphic_info = widgets.Textarea(description="Map stratigraphic elements"),
 
-        output_scale = widgets.Text(description="Output scale"),
-        crs = widgets.Textarea(description="Original CRS"),
+#         output_scale = widgets.Text(description="Output scale"),
+#         crs = widgets.Textarea(description="Original CRS"),
 
-        ancillary_data = widgets.Textarea(description="Data used"),
-        related_products = widgets.Textarea(description="Related maps"),
-        heritage = widgets.Textarea(description="Heritage information"),
-        extra_data = widgets.Textarea(description="Links to other data"),
+#         ancillary_data = widgets.Textarea(description="Data used"),
+#         related_products = widgets.Textarea(description="Related maps"),
+#         heritage = widgets.Textarea(description="Heritage information"),
+#         extra_data = widgets.Textarea(description="Links to other data"),
 
-        standards = widgets.Textarea(description="Standards"),
-        comments = widgets.Textarea(description="Notes, errata, caveats"),
-        acknowledge = widgets.Textarea(description="Acknowledgement"),
-    )
+#         standards = widgets.Textarea(description="Standards"),
+#         comments = widgets.Textarea(description="Notes, errata, caveats"),
+#         acknowledge = widgets.Textarea(description="Acknowledgement"),
+#     )
 
-    return d
+#     return d
 
---
+++ api/gui/schema2ipywidgets.py
@@ -0,0 +1,232 @@
+import ipywidgets as widgets
+
+
+def main(obj:dict) -> dict:
+    """Return a representation of 'obj' schema with widgets to be used in Form"""
+    assert isinstance(obj, dict), obj
+    assert obj['type'] == 'object', obj
+    # res = _handlers[obj['type']](obj)
+    res = _object(obj)
+    return res
+
+
+def _object(obj:dict, ) -> dict:
+    """
+    Process an 'object' element
+
+    >>> _object({
+            "type": "object",
+            "properties": {
+                "name": {"type": "string"},
+                "type": {"const": "dataset"},
+                "date": {"type": "string", "format": "date"}
+            },
+            "required": ["name"]
+        })
+    """
+    # check mandatory fields
+    mandatory_fields = ['type', 'properties']
+    assert all([ k in obj for k in mandatory_fields ])
+    assert obj['type'] == 'object'
+
+    # desc = obj['description']
+
+    props = _properties(obj['properties'],
+                        required = obj.get('required', None))
+    return props
+
+
+def _array(obj:dict, name:str) -> list:
+    """
+    Return a container for items' Params
+    """
+    # check mandatory fields
+    assert all([ k in obj for k in ['type', 'items'] ]), obj
+    assert obj['type'] == 'array', obj
+
+    return _items(obj['items'], name, minItems = obj.get('minItems', None))
+
+
+def _items(obj:dict, name:str, minItems=None, maxItems=None):
+    """
+    Return a list of "param types"
+    """
+    assert 'type' in obj
+    assert obj['type'] != 'array'
+    assert maxItems is None, "'maxItems' is not implemented yet"
+
+    if 'enum' in obj:
+        return widgets.SelectMultiple(
+            options = obj['enum'],
+            description = name.replace('_',' ').title()
+        )
+
+    if 'type' in obj:
+        # return Items().widget
+        return Items()
+
+
+def _string(obj:dict, name:str):
+    """ 
+    Return a Text iPywidget
+    """
+    name = name.replace('_', ' ').title()
+
+    if 'enum' in obj:
+        return widgets.Dropdown(
+            description = name,
+            options = obj['enum']
+        )
+
+    return widgets.Text(
+        description = name
+    ) 
+
+
+def _number(obj:dict, name:str):
+    """
+    Return a FloatText iPywidget
+    """
+    return widgets.FloatText(
+        description = name.replace('_', ' ').title()
+    )
+
+
+_type_resolvers = {
+    'string': _string,
+    'number': _number,
+    'array': _array,
+    'object': _object,
+}
+
+_handlers = _type_resolvers
+
+
+def _properties(obj:dict, required:list = None) -> dict:
+    """
+    >>> _properties({"name": {"type": "string"}, "type": {"const": "dataset"}, "date": {"type": "string", "format": "date"}})
+    """
+    required = set(required) if required else set()
+    _props = {}
+    for name, prop_obj in obj.items():
+        _props[name] = _property(prop_obj, name, required=name in required)
+
+    return _props
+
+
+def _property(schema_property:dict, name:str, required:bool = False):
+    """
+    Return a "Param(*args, **kwargs)" object according to "obj['type']"
+
+    >>> _property( {"type":"string"} )
+    """
+    obj = schema_property
+
+    # check supported attributes
+    supported_fields = ('type', 'oneOf')
+    assert any([ k in obj for k in supported_fields ]), obj
+
+    if 'type' in obj:
+        param_obj = _type_resolvers[obj['type']](obj, name)
+
+    else:
+        assert 'oneOf' in obj, obj
+        param_objs = [ _type_resolvers[o['type']](o) for o in obj['oneOf'] ]
+        # param_obj = param.Selector(param_objs)
+        # param_obj = param_objs
+        param_obj = { 'oneOf': param_objs }
+
+    return param_obj
+
+
+def _date(val, *args) -> tuple:
+    """
+    Return 'val' (and *args) as datetime.date object if not yet (eg, 'YYYY-MM-DD')
+    """
+    import datetime
+
+    if val:
+        assert isinstance(val, (str, datetime.date))
+        val = datetime.date.fromisoformat(val) if isinstance(val, str) else val
+    else:
+        # val = datetime.date.today()
+        val = val
+
+    return (val, *args)
+
+
+_format_args = {
+    "date": _date
+}
+
+_wdgt_classes = {
+    "string": widgets.Text,
+    "date": widgets.DatePicker
+}
+
+def _param(obj_type, *args, **kwargs):
+    """
+    Return Widgets object according to '_param_classes' and 'obj_type'.
+
+    Input:
+        *args : 
+    """
+    print(args, kwargs)
+    wdgt_class = _wdgt_classes[obj_type]
+    _format = _format_args.get(obj_type)
+    args = _format(*args) if _format else args
+    return wdgt_class(*args, **kwargs)
+
+
+@widgets.register
+class Items(widgets.VBox):
+
+    def __init__(self, description="Text field"):
+        self.description = description
+        add_btn = widgets.Button(icon="plus")
+        add_btn.on_click(lambda btn: self.add_item())
+        super().__init__([add_btn])
+        
+
+    def del_item(self, value):
+        def not_value(item):
+            try:
+                found = item.children[0].value == value
+            except:
+                found = False
+            return not found
+        
+        new_items = filter(not_value, self.children)
+        self.children = tuple(new_items)
+
+        return self
+
+
+    def add_item(self, value=None):
+        """Create new Text input widget in items"""
+        text = widgets.Text(description = self.description)
+        if value is not None:
+            text.value = str(value)
+
+        del_btn = widgets.Button(icon="trash")
+        del_btn.on_click(lambda btn: self.del_item(text.value))
+
+        new_item = widgets.HBox([text, del_btn])
+        
+        if all(item.children[0].value.strip() 
+               for item in self.children if hasattr(item, 'children')):
+            self.children = tuple(list(self.children) + [new_item])
+
+        return self
+        
+        
+    @property 
+    def value(self):
+        value = [w.children[0].value for w in self.children[1:]]
+        return value 
+
+    @value.setter
+    def value(self, value:list):
+        for val in value:
+            self.add_item(val)
+
